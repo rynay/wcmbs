561096c62fb536ae8745aaef8d5ffc03b442bd2f
rynay <feirs911@gmail.com>
2021-08-22 09:58:20 +0300

    Header: done( without adaptive )

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,10 @@ import { Link as RLink, NavLink } from "react-router-dom";
 import { Link as SLink } from "react-scroll";
 import s from "./Header.module.scss";
 import { pages } from "../../data/pages";
+import { Facebook } from "../icons/Facebook";
+import { Logo } from "../icons/Logo";
+import { Twitter } from "../icons/Twitter";
+import { Instagram } from "../icons/Instagram";
 
 type Props =
   | { type: "header"; page: "main" }
@@ -11,7 +15,12 @@ type Props =
 export const Header = ({ type, page }: Props) => {
   return (
     <>
-      <header id="top" className={s.header}>
+      <header
+        id={type === "header" ? "top" : "bottom"}
+        className={`${s.header} ${
+          type === "header" ? s.header_top : s.header_bottom
+        }`}
+      >
         <div
           className={
             page === "main" ? s.header__container : s.header__container_maxWidth
@@ -19,10 +28,7 @@ export const Header = ({ type, page }: Props) => {
         >
           <div className={s.header__left}>
             <RLink className={s.header__logoLink} to="/">
-              <img
-                src="/images/mountain-bike.svg"
-                alt="West Coast Moutain Bike School Logo"
-              />
+              <Logo />
             </RLink>
           </div>
           <div className={s.header__middle}>
@@ -30,7 +36,9 @@ export const Header = ({ type, page }: Props) => {
               <ul>
                 {pages.map((page) => (
                   <li key={page.anchor}>
-                    <NavLink to={page.path}>{page.anchor}</NavLink>
+                    <NavLink className={s.header__navLink} to={page.path}>
+                      {page.anchor}
+                    </NavLink>
                   </li>
                 ))}
               </ul>
@@ -46,13 +54,13 @@ export const Header = ({ type, page }: Props) => {
             {type === "header" && (
               <div className={s.header__socials}>
                 <a href="#1" aria-label="our facebook">
-                  <img src="/images/facebook.svg" alt="facebook logo" />
+                  <Facebook />
                 </a>
                 <a href="#2" aria-label="our twitter">
-                  <img src="/images/twitter.svg" alt="twitter logo" />
+                  <Twitter />
                 </a>
                 <a href="#3" aria-label="our instagram">
-                  <img src="/images/instagram.svg" alt="instagram logo" />
+                  <Instagram />
                 </a>
               </div>
             )}
---
+++ src/components/icons/Facebook/Facebook.tsx
@@ -0,0 +1,32 @@
+export const Facebook = () => {
+  return (
+    <svg
+      enable-background="new 0 0 511.992 511.992"
+      height="512"
+      viewBox="0 0 511.992 511.992"
+      width="512"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <g>
+          <path d="M288,176v-64c0-17.664,14.336-32,32-32h32V0h-64c-53.024,0-96,42.976-96,96v80h-64v80h64v256h96V256h64l32-80H288z" />
+        </g>
+      </g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+    </svg>
+  );
+};
